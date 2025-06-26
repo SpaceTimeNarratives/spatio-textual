7cedb92cf0de1682ab78740161aa96364e7289ae
Ignatius Ezeani <igezeani@yahoo.com>
2025-06-26 23:33:58 +0100

    fix default model to `encorewebsm`

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -1,31 +1,3 @@
-# import unittest
-# import warnings
-# warnings.simplefilter("ignore", category=FutureWarning)
-
-# from spatio_textual.annotate import annotate_text
-
-# class TestAnnotateText(unittest.TestCase):
-#     def test_basic_annotation(self):
-#         text = "Anne Frank was taken from Amsterdam to Auschwitz."
-#         result = annotate_text(text)
-#         print(f"{10*'-'}\n{result}\n{10*'-'}")
-
-#         self.assertIsInstance(result, dict)  # or list, depending on your implementation
-#         self.assertIn("entities", result)    # Adjust based on the structure of your output
-
-#         expected_entities, verb_data = [
-#             {'start_char': 0,  'token': "Anne Frank", 'tag': "PERSON"},
-#             {'start_char': 26, 'token': "Amsterdam",  'tag': "CITY"},
-#             {'start_char': 39, 'token': "Auschwitz",  'tag': "CAMP"},
-#             ], [{'sent-id': 0, 'verb': 'taken', 'subject': 'Anne Frank', 'object': 'Amsterdam', 
-#                  'sentence': 'Anne Frank was taken from Amsterdam to Auschwitz.'}]
-
-#         self.assertEqual(result["entities"], expected_entities)
-#         self.assertEqual(result["verb_data"], verb_data)
-
-# if __name__ == '__main__':
-#     unittest.main()
-
 import unittest
 import json
 from pathlib import Path
